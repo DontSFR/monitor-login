87aa17f3bcaa83c1542cc7ac74123e4ef466ddb1
fanyidong <fanyidong@biosan.cn>
2018-12-12 13:09:37 +0800

    更新登录接口参数&新增获取验证码接口

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 1. 登录接口
     1. url地址：http://39.108.52.40:8002/user/login
     2. 请求方式：post
-    3. 参数：{"account": "root","password": "1", "captcha": ""}
+    3. 参数：{"params":{"account": "root","password": "1", "captcha": ""}}
     4. 返回格式：{"result":"success","message":null,"data":null}
 
+2. 获取验证码
+    1. url地址：http://39.108.52.40:8002/RandomCode
+    2. 请求方式：get
+    3. 参数：无
+    4. 返回格式：JPEG格式图片(60*22)
--- src/main/java/cn/fyd/monitorlogin/MonitorLoginApplication.java
+++ src/main/java/cn/fyd/monitorlogin/MonitorLoginApplication.java
@@ -3,9 +3,8 @@ package cn.fyd.monitorlogin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 启动类
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @MapperScan("cn.fyd.monitorlogin.dao")
+@ServletComponentScan("cn.fyd.monitorlogin.servlet")
 public class MonitorLoginApplication {
 
     public static void main(String[] args) {
