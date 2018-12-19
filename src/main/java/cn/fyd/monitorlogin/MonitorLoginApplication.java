7cd85963a5db6b1f99493b62831bb9d1a5f9e01f
fanyidong <fanyidong@biosan.cn>
2018-12-19 15:49:56 +0800

    修改打包方式为jar

--- src/main/java/cn/fyd/monitorlogin/MonitorLoginApplication.java
+++ src/main/java/cn/fyd/monitorlogin/MonitorLoginApplication.java
@@ -3,9 +3,7 @@ package cn.fyd.monitorlogin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -18,19 +16,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @MapperScan("cn.fyd.monitorlogin.dao")
 @ServletComponentScan("cn.fyd.monitorlogin.servlet")
-public class MonitorLoginApplication extends SpringBootServletInitializer {
+public class MonitorLoginApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonitorLoginApplication.class, args);
-    }
-
-    /**
-     * 构建war包 部署 需要
-     * @param builder
-     * @return
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MonitorLoginApplication.class);
     }
 }
